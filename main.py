4ba8429725732732c9d0e6615683fd8394ef7aea
Mazen Tarek <mazen.hanafy.ext@siemens.com>
2026-06-14 13:38:59 +0300

    add host app and client session

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from mcp.client.stdio import stdio_client
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 
 
 load_dotenv()
@@ -21,12 +22,21 @@ llm = ChatGoogleGenerativeAI(
 
 stdio_server_params = StdioServerParameters(
     command="python",
-    args=["/Users/Z005566R/Desktop/AI Projects/MCP/servers/math_server.py"]
+    args=["/servers/math_server.py"]
 )
 
 
 async def main():
-    print("Hello from mcp!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session Initialized")
+            tools = await load_mcp_tools(session)
+            print(tools)
+            agent = create_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 2+2")]})
+            print(result["messages"][-1].content)
+
 
 
 if __name__ == "__main__":
